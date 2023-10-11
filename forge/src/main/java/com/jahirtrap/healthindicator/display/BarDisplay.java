e02cae99d4dc285e73c0f100bb7803edb4f69c20
jahirxtrap <jahirroman2002@gmail.com>
2023-10-11 05:45:26 -0500

    Health decimals, bar styles

--- forge/src/main/java/com/jahirtrap/healthindicator/display/BarDisplay.java
+++ forge/src/main/java/com/jahirtrap/healthindicator/display/BarDisplay.java
@@ -10,8 +10,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 
-import static com.jahirtrap.healthindicator.util.CommonUtils.getColor;
-import static com.jahirtrap.healthindicator.util.CommonUtils.getModName;
+import static com.jahirtrap.healthindicator.util.CommonUtils.*;
 
 public class BarDisplay {
     private static final ResourceLocation ARMOR_FULL_SPRITE = new ResourceLocation("hud/armor_full");
@@ -31,6 +30,10 @@ public class BarDisplay {
         int barWidth = 128, barHeight = 6;
         int xOffset = 1, xOffsetM = 1;
 
+        switch (HealthIndicatorModConfig.barStyle) {
+            case VANILLA -> barHeight = 5;
+        }
+
         int armorValue = entity.getArmorValue();
         boolean armor = armorValue > 0;
 
@@ -39,15 +42,19 @@ public class BarDisplay {
         else barHeight = 0;
 
         String name = getEntityName(entity);
-        int healthMax = Mth.ceil(entity.getMaxHealth());
-        int healthCur = Math.min(Mth.ceil(entity.getHealth()), healthMax);
+        String healthMax = String.valueOf(Mth.ceil(entity.getMaxHealth()));
+        String healthCur = String.valueOf(Math.min(Mth.ceil(entity.getHealth()), Integer.parseInt(healthMax)));
+        if (HealthIndicatorModConfig.showHealthDecimals) {
+            healthMax = formatText(entity.getMaxHealth());
+            healthCur = formatText(Math.min(entity.getHealth(), Float.parseFloat(healthMax)));
+        }
         String healthText = healthCur + "/" + healthMax;
         String armorText = String.valueOf(armorValue);
         String modNameText = getModName(entity);
 
         switch (HealthIndicatorModConfig.healthTextFormat) {
-            case CURRENT_HEALTH -> healthText = String.valueOf(healthCur);
-            case MAX_HEALTH -> healthText = String.valueOf(healthMax);
+            case CURRENT_HEALTH -> healthText = healthCur;
+            case MAX_HEALTH -> healthText = healthMax;
         }
 
         int offAux = 0;
