ba8b74e50b2daba761584e022d2ae021f2a767d0
jahirxtrap <jahirroman2002@gmail.com>
2024-04-27 18:36:10 -0500

    Some fixes

--- forge/src/main/java/com/jahirtrap/healthindicator/init/DamageParticleEvents.java
+++ forge/src/main/java/com/jahirtrap/healthindicator/init/DamageParticleEvents.java
@@ -26,7 +26,7 @@ public class DamageParticleEvents {
 
     @OnlyIn(Dist.CLIENT)
     @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void onLivingUpdateEvent(final LivingTickEvent event) {
+    public static void onLivingTick(final LivingTickEvent event) {
         if (!HealthIndicatorModConfig.showDamageParticles || !HealthIndicatorModConfig.enableMod) return;
         LivingEntity livingEntity = event.getEntity();
         if (checkBlacklist(HealthIndicatorModConfig.blacklist, livingEntity) || checkBlacklist(HealthIndicatorModConfig.damageParticleBlacklist, livingEntity)) return;
