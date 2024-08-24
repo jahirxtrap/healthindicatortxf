edf49f0cd4efc356b2b088ee841c98cf77487401
jahirxtrap <jahirroman2002@gmail.com>
2024-08-24 01:10:34 -0500

    Update config lib

--- fabric/src/main/java/com/jahirtrap/healthindicator/display/BarDisplay.java
+++ fabric/src/main/java/com/jahirtrap/healthindicator/display/BarDisplay.java
@@ -1,7 +1,7 @@
 package com.jahirtrap.healthindicator.display;
 
 import com.jahirtrap.healthindicator.bars.HealthBarRenderer;
-import com.jahirtrap.healthindicator.init.HealthIndicatorModConfig;
+import com.jahirtrap.healthindicator.init.ModConfig;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
@@ -31,7 +31,7 @@ public class BarDisplay {
         int barWidth = 128, barHeight = 6;
         int xOffset = 1, xOffsetM = 1, yOffset = 2;
 
-        switch (HealthIndicatorModConfig.barStyle) {
+        switch (ModConfig.barStyle) {
             case VANILLA -> barHeight = 5;
         }
 
@@ -47,7 +47,7 @@ public class BarDisplay {
         float maxHealth = entity.getMaxHealth() + entity.getAbsorptionAmount();
         String healthMax = String.valueOf(Mth.ceil(maxHealth));
         String healthCur = String.valueOf(Math.min(Mth.ceil(health), Integer.parseInt(healthMax)));
-        if (HealthIndicatorModConfig.showHealthDecimals) {
+        if (ModConfig.showHealthDecimals) {
             healthMax = formatText(maxHealth);
             healthCur = formatText(Math.min(health, Float.parseFloat(healthMax)));
         }
@@ -55,16 +55,16 @@ public class BarDisplay {
         String armorText = String.valueOf(armorValue);
         String modNameText = getModName(entity);
 
-        switch (HealthIndicatorModConfig.healthTextFormat) {
+        switch (ModConfig.healthTextFormat) {
             case CURRENT_HEALTH -> healthText = healthCur;
             case MAX_HEALTH -> healthText = healthMax;
         }
 
-        boolean showName = HealthIndicatorModConfig.showName;
-        boolean showHealth = HealthIndicatorModConfig.showHealth;
-        boolean showArmor = HealthIndicatorModConfig.showArmor;
-        boolean showModName = HealthIndicatorModConfig.showModName;
-        boolean showBar = HealthIndicatorModConfig.showBar;
+        boolean showName = ModConfig.showName;
+        boolean showHealth = ModConfig.showHealth;
+        boolean showArmor = ModConfig.showArmor;
+        boolean showModName = ModConfig.showModName;
+        boolean showBar = ModConfig.showBar;
 
         setInfoWidth(name, armor, healthText, armorText);
         int offAux = getInfoWidth();
@@ -74,7 +74,7 @@ public class BarDisplay {
         int centerM = (barWidth / 2) - ((mc.font.width(modNameText)) / 2);
         int rightM = barWidth - (mc.font.width(modNameText)) - xOffsetM;
 
-        switch (HealthIndicatorModConfig.position) {
+        switch (ModConfig.position) {
             case BOTTOM_CENTER, TOP_CENTER -> {
                 xOffset = center;
                 xOffsetM = centerM;
@@ -109,7 +109,7 @@ public class BarDisplay {
             yOffset = 15 + barHeight;
             if (offAux == 0) yOffset -= 12;
             if (!showBar) yOffset -= barHeight + 2;
-            guiGraphics.drawString(mc.font, modNameText, xOffsetM, yOffset, getColor(0x5555ff, HealthIndicatorModConfig.modNameColor));
+            guiGraphics.drawString(mc.font, modNameText, xOffsetM, yOffset, getColor(0x5555ff, ModConfig.modNameColor));
         }
     }
 
@@ -123,7 +123,7 @@ public class BarDisplay {
         RenderSystem.setShaderTexture(0, ICON_TEXTURES);
         guiGraphics.blit(ICON_TEXTURES, x, 1, 16, 0, 9, 9);
 
-        if (HealthIndicatorModConfig.dynamicHeartTexture) {
+        if (ModConfig.dynamicHeartTexture) {
             // if (WITHER) u = 124; if (POISON) u = 88;
             if (entity instanceof Player && mc.level != null && mc.level.getLevelData().isHardcore()) v = 45;
             if (entity.getTicksFrozen() >= entity.getTicksRequiredToFreeze()) u = 178;
@@ -135,19 +135,19 @@ public class BarDisplay {
     public void setInfoWidth(String name, boolean armor, String healthText, String armorText) {
         int infoWidth = 0;
         boolean aux = true;
-        if (HealthIndicatorModConfig.showName && !name.isBlank()) {
+        if (ModConfig.showName && !name.isBlank()) {
             infoWidth += mc.font.width(name);
-            if (HealthIndicatorModConfig.showHealth) {
+            if (ModConfig.showHealth) {
                 infoWidth += 5;
                 aux = false;
             }
-            if (armor && HealthIndicatorModConfig.showArmor) infoWidth += 5;
+            if (armor && ModConfig.showArmor) infoWidth += 5;
         }
-        if (HealthIndicatorModConfig.showHealth) {
+        if (ModConfig.showHealth) {
             infoWidth += mc.font.width(healthText) + 10;
-            if (armor && HealthIndicatorModConfig.showArmor && aux) infoWidth += 5;
+            if (armor && ModConfig.showArmor && aux) infoWidth += 5;
         }
-        if (armor && HealthIndicatorModConfig.showArmor) infoWidth += mc.font.width(armorText) + 10;
+        if (armor && ModConfig.showArmor) infoWidth += mc.font.width(armorText) + 10;
         this.infoWidth = infoWidth;
     }
 
