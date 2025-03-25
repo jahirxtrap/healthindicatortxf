d8ef1315a85802f4d2bf04f709ba7659d28f2f6b
jahirxtrap <jahirroman2002@gmail.com>
2025-03-25 17:13:34 -0500

    Fix

--- fabric/src/main/java/com/jahirtrap/healthindicator/HealthIndicatorMod.java
+++ fabric/src/main/java/com/jahirtrap/healthindicator/HealthIndicatorMod.java
@@ -1,17 +1,12 @@
 package com.jahirtrap.healthindicator;
 
 import com.jahirtrap.configlib.TXFConfig;
-import com.jahirtrap.healthindicator.display.Hud;
 import com.jahirtrap.healthindicator.init.ModConfig;
-import com.jahirtrap.healthindicator.util.RayTrace;
 import net.fabricmc.api.ModInitializer;
 
 public class HealthIndicatorMod implements ModInitializer {
 
     public static final String MODID = "healthindicatortxf";
-
-    public static Hud HUD = new Hud();
-    public static RayTrace RAYTRACE = new RayTrace();
 
     @Override
     public void onInitialize() {
